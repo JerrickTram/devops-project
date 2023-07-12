0c5ea97020c2ed51834020116e20ed8b8567db53
JerrickTram <jerricktram8@gmail.com>
2023-07-12 17:58:31 -0400

    condensed variables with object

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 provider "google" {
   project     = var.project_id
   credentials = "${file(var.credentials)}"
-  region      = var.vm_params[1]
-  zone        = var.vm_params[2]
+  region      = var.vm_params.region
+  zone        = var.vm_params.zone
 }
 
 resource "google_compute_instance" "test-node-" {
   count                     = var.node_count
   name                      = "test-node-${count.index}"
-  machine_type              = var.vm_params[0]
-  zone                      = var.vm_params[2]
-  allow_stopping_for_update = var.vm_params[3]
+  machine_type              = var.vm_params.machine_type
+  zone                      = var.vm_params.zone
+  allow_stopping_for_update = var.vm_params.allow_stopping_for_update
 
   boot_disk {
     initialize_params {
@@ -36,6 +36,6 @@ resource "google_compute_network" "terraform_network" {
 resource "google_compute_subnetwork" "terraform_subnet" {
     name                        = "terraform-subnetwork"
     ip_cidr_range               = "10.202.0.0/20"
-    region                      = var.vm_params[2]
+    region                      = var.vm_params.region
     network                     = google_compute_network.terraform_network.id
 }
--- variables.tf
+++ variables.tf
@@ -6,9 +6,26 @@ variable "credentials" {
 }
 
 variable "vm_params" {
-    type = tuple({string, string, string, bool})
+    type        = object({
+        name                        = string
+        machine_type                = string
+        region                      = string
+        zone                        = string
+        allow_stopping_for_update   = bool
+    })
     description = "vm parameters"
-    default = ["e2-small", "us-east5", "us-east5-a", true]
+    default     = {
+        name                        = "terraform-instance"
+        machine_type                = "e2-small"
+        region                      = "us-east5"
+        zone                        = "us-east5-a"
+        allow_stopping_for_update   = true
+    }
+
+    validation {
+        condition       = length(var.vm_params.name) > 3
+        error_message   = "VM name must be longer than 3 characters."
+    }
 }
 
 variable "node_count" {
